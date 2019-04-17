13bf85a75898a12d648856649aa5c45c6d95ae84
lampert309 <lampert309@gmail.com>
2019-04-17 08:21:59 -0700

    Finished classes Responce, and RandomResponce, closing issues #18, and #20

--- ChatBot/Responce.cs
+++ ChatBot/Responce.cs
@@ -7,11 +7,17 @@ namespace ChatBot
 {
     public class Responce
     {
-
+        public string Input { get; set; }
+        public string Output { get; set; }
     }
 
     public class RandomResponce
     {
-
+        public Responce GetResponce(List<Responce> responces)
+        {
+            Random rand = new Random();
+            int index = rand.Next(0, responces.Count);
+            return responces[index];
+        }
     }
 }
