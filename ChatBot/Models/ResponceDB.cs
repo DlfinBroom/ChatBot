15e58a7e4bf7fa2821215a1fbb4049089c4491a3
lampert309 <lampert309@gmail.com>
2019-05-06 08:10:19 -0700

    Moved Responce and ResponceDB inside of the models folder, and added all of the empty methods inside of ResponceDB

---
+++ ChatBot/Models/ResponceDB.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChatBot
+{
+    public class ResponceDB
+    {
+        public List<Responce> GetSomeResponces(string input)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Responce> GetAllResponces()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool AddResponce(Responce res)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DeleteResponce(Responce res)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UpdateResponce(Responce oldRes, Responce newRes)
+        {
+            if (DeleteResponce(oldRes))
+            {
+                return AddResponce(newRes);
+            }
+            return false;
+        }
+    }
+}
