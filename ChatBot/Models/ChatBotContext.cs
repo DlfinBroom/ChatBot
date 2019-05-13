f85d33913fb390398e49c9c735c88d8da301b279
lampert309 <lampert309@gmail.com>
2019-05-13 14:16:48 -0700

    Added a ChatBotContext class, and added the chatbotdb inside of the appsettings.json, clossing issue #39

---
+++ ChatBot/Models/ChatBotContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChatBot.Models
+{
+    public class ChatBotContext : DbContext
+    {
+        public ChatBotContext (DbContextOptions<ChatBotContext> options) : base(options) { }
+
+        public DbSet<Responce> Responce { get; set; }
+    }
+}
